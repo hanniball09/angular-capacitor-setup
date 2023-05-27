f71b68fb4420b13fe748dd7dc6d3f681ea59e0b5
hanniball09 <134778298+hanniball09@users.noreply.github.com>
2023-05-27 14:35:08 +0200

    added login component

--- dogs/src/app/app.module.ts
+++ dogs/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { XyzComponent } from './xyz/xyz.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent
+    XyzComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
